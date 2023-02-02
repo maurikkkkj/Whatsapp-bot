3b96b6098624a9f839a7ce98b56734a3fc83f2b9
mauri <78458084+maurikkkkj@users.noreply.github.com>
2023-02-02 07:53:44 -0800

    OK
    AAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const wa = require('@open-wa/wa-automate');
+const wazap = require('@open-wa/wa-automate');
 const axios = require('axios');
 const fs = require('fs');
 
-wa.create({
+wazap.create({
   sessionId: "WhatsApp",
   multiDevice: true,
   authTimeout: 60,
@@ -17,9 +17,14 @@ wa.create({
 
 function start(client) {
 client.onMessage(async message => {
+ //INICIAMENTO PARA CRIAR SEUS COMANDOS!
 if (message.body === "pensamento suicidas") {
 const gif4 = await fs.readFileSync('./lib/pensando.webp', { encoding: 'base64' });
 await client.sendImageAsSticker(message.from, `data:image/gif;base64,${gif4.toString('base64')}`);
+}
+  
+if (message.body === "sou gay") {
+await client.reply(message.from, `já sabia!`)
 }
     }
   });
